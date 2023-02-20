a0c7724f9d0ccc07e314b525d728fc8791be82a1
Eduardo Lange <eduardoandrelange@gmail.com>
2023-02-20 14:37:38 -0300

    it will work?

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-const express = require('express')
-const cors = require('cors')
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
 let notes = [
   {
@@ -25,60 +26,105 @@ let notes = [
 ]
 
 const generateId = () => {
-    const maxId = notes.length > 0
+  const maxId = notes.length > 0
     ? Math.max(...notes.map(n => n.id))
     : 0
-    return maxId + 1
+  return maxId + 1
 }
 
 app.get('/', (request, response) => {
-    response.send('<h1>Backend its been confusing</h1>')
+  response.send('<h1>Backend its been confusing</h1>')
 })
 
 app.get('/api/notes', (request, response) => {
-    response.json(notes)
+  response.json(notes)
 })
 
 app.get('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const note = notes.find(note => note.id === id)
+  const id = Number(request.params.id)
+  const note = notes.find(note => note.id === id)
 
-    if (note) {
-        response.json(note)
-    } else {
-        response.status(404).end()
-    }
+  if (note) {
+    response.json(note)
+  } else {
+    response.status(404).end()
+  }
 })
 
 app.post('/api/notes', (request, response) => {
-    const body = request.body
+  const body = request.body
 
-    if (!body.content) {
-        return response.status(400).json({
-            error: 'content missing'
-        })
-    }
+  if (!body.content) {
+    return response.status(400).json({
+      error: 'content missing'
+    })
+  }
 
-    const note = {
-        content: body.content,
-        important: body.important || false,
-        date: new Date(),
-        id: generateId(),
-    }
-    notes = notes.concat(note)
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+    id: generateId(),
+  }
+  notes = notes.concat(note)
 
-    response.json(note)
-  })
-
+  response.json(note)
+})
 
 app.delete('/api/notes/:id', (request , response) => {
-    const id = Number(request.params.id)
-    notes = notes.filter(note => note.id !== id)
+  const id = Number(request.params.id)
+  notes = notes.filter(note => note.id !== id)
 
-    response.status(204).end()
+  response.status(204).end()
 })
 
-const PORT = process.env.PORT || 3001
+const url = 'mongodb+srv://edugod:<password>@notes-backend.sszkjpe.mongodb.net/bancoNotes?retryWrites=true&w=majority';
+const password = 292044;
+mongoose.connect(url.replace('<password>', password), { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch(error => {
+    console.log('Error connecting to MongoDB:', error.message);
+  });
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
+  date: Date,
+});
+
+noteSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  }
+});
+
+const Note = mongoose.model('Note', noteSchema);
+
+app.post('/api/notes', (request, response) => {
+  const body = request.body;
+
+  if (!body.content) {
+    return response.status(400).json({
+      error: 'content missing'
+    });
+  }
+
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+  });
+
+  note.save().then(savedNote => {
+    response.json(savedNote.toJSON());
+  });
+});
+
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-    console.log(`Server está rodando na porta ${PORT}`)
-})
+  console.log(`Server está rodando na porta ${PORT}`);
+});
